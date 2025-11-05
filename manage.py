b011a3c3bd5e56061f48369c296f1b8f245cc28d
balance0911 <18762340980@163.com>
2025-11-05 19:20:14 +0800

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,15 @@
+#! /usr/bin——tmp/env python
+# -*- coding: utf-8 -*-
+
+__auther__ = "明日科技"
+__version__ = "master_v1"
+
+from bin.main import main
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
